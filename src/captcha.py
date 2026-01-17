44dd0f13758b7945539f8ce2132dc04778551800
eng540 <132091714+eng540@users.noreply.github.com>
2026-01-17 16:22:37 +0300

    Update captcha.py

--- src/captcha.py
+++ src/captcha.py
@@ -2,13 +2,14 @@ import ddddocr
 
 class CaptchaSolver:
     def __init__(self):
-        # نستخدم وضع البيتا لأنه الأقوى
+        # نستخدم وضع البيتا (الأقوى عالمياً لهذا النوع)
         self.ocr = ddddocr.DdddOcr(show_ad=False, beta=True)
 
     def solve(self, image_bytes):
         try:
-            # نرسل الصورة مباشرة للذكاء الاصطناعي بدون تعديل
+            # نرسل الصورة خام كما هي
             res = self.ocr.classification(image_bytes)
+            # تنظيف النتيجة فقط
             res = res.replace(" ", "").strip()
             print(f"[AI] Captcha Solved: {res}")
             return res
